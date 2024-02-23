e5b0076130caa3a21f7b4d8c4a477d78936f1832
MoodyEG <moodyeg2011@yahoo.com>
2024-02-23 11:38:42 -0800

    done fast

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - infinite loop
+ * 
+ * Return: Always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - Main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			return (0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
